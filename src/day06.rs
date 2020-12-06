2ef4b2cba360386927ca1d39ea10d3a12b064e08
marcusdunn <marcus.s.dunn@gmail.com>
2020-12-06 01:06:29 -0800

    fixed warnings

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,5 @@
 mod day06 {
-    use std::collections::{HashMap, HashSet};
+    use std::collections::{HashMap};
     use std::str::FromStr;
 
     #[derive(Debug)]
@@ -10,9 +10,7 @@ mod day06 {
 
     impl Group {
         pub fn amount_all_yeses(&self) -> usize {
-            self.yeses.iter().filter(|(_, i)| {
-                self.size.eq(i)
-            }).count()
+            self.yeses.iter().filter(|(_, i)| self.size.eq(i)).count()
         }
     }
 
@@ -21,10 +19,10 @@ mod day06 {
 
         fn from_str(s: &str) -> Result<Self, Self::Err> {
             let size = s.chars().filter(|c| c == &' ').count() + 1;
-            let filtered= s.chars().filter(|c| c != &' ').collect::<Vec<char>>();
+            let filtered = s.chars().filter(|c| c != &' ').collect::<Vec<char>>();
             let mut yeses = HashMap::new();
             for char in filtered {
-                if let Some(amount) = yeses.get(&char) {
+                if let Some(&amount) = yeses.get(&char) {
                     yeses.insert(char, amount + 1);
                 } else {
                     yeses.insert(char, 1);
@@ -42,15 +40,23 @@ mod day06test {
 
     #[test]
     fn test_small() {
-        let input = file_to_vec_by_blank_lines::<Group>("inputs/day06small.txt");
-        let result = input.iter().fold(0, |acc, x| acc + x.amount_all_yeses());
-        println!("small {}", result);
+        let result = timed!(
+            file_to_vec_by_blank_lines::<Group>("inputs/day06small.txt")
+                .iter()
+                .fold(0, |acc, x| acc + x.amount_all_yeses()),
+            "test_small"
+        );
+        assert_eq!(result, 6)
     }
 
     #[test]
     fn test_large() {
-        let input = file_to_vec_by_blank_lines::<Group>("inputs/day06.txt");
-        let result = input.iter().fold(0, |acc, x| acc + x.amount_all_yeses());
-        println!("large {}", result);
+        let result = timed!(
+            file_to_vec_by_blank_lines::<Group>("inputs/day06.txt")
+                .iter()
+                .fold(0, |acc, x| acc + x.amount_all_yeses()),
+            "test_large"
+        );
+        assert_eq!(result, 3290)
     }
 }
