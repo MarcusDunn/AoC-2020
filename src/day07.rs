02f3f6ad2e9a5e8adb7bcf787e8a70cf05d187af
marcusdunn <marcus.s.dunn@gmail.com>
2020-12-07 20:36:32 -0800

    added benchmarking to shame me for bad code

--- src/day07.rs
+++ src/day07.rs
@@ -1,9 +1,9 @@
 mod day07 {
-    use std::collections::{HashMap, HashSet};
+    use std::collections::HashSet;
     use std::hash::Hash;
     use std::str::FromStr;
 
-    #[derive(Hash, Eq, PartialEq, Debug)]
+    #[derive(Hash, Eq, PartialEq)]
     pub struct BagType {
         adj: String,
         color: String,
@@ -34,7 +34,6 @@ mod day07 {
         }
     }
 
-    #[derive(Debug)]
     pub struct Bag {
         bag_type: BagType,
         inner: Vec<(BagType, i32)>,
@@ -81,43 +80,39 @@ mod day07 {
         }
     }
 
-    #[derive(Debug)]
     pub struct HashMatrix<'a> {
-        matrix: HashMap<(&'a BagType, &'a BagType), i32>,
+        matrix: Vec<(&'a BagType, &'a BagType, i32)>,
     }
 
     impl<'a> HashMatrix<'a> {
         pub fn new() -> HashMatrix<'a> {
-            HashMatrix {
-                matrix: HashMap::new(),
-            }
+            HashMatrix { matrix: Vec::new() }
         }
 
         pub fn add(&mut self, bag: &'a Bag) {
             let Bag { bag_type, inner } = bag;
             for (inner_bag, num) in inner {
-                self.matrix.insert((&bag_type, &inner_bag), *num);
+                self.matrix.push((&bag_type, &inner_bag, *num));
             }
         }
 
         pub fn bags_that_contain(&self, bag_type: &BagType) -> HashSet<&BagType> {
             self.matrix
-                .keys()
-                .filter(|(_, contents)| bag_type.eq(contents))
-                .fold(HashSet::new(), |mut acc, (bag, _)| {
-                    for x in self.bags_that_contain(bag) {
-                        acc.insert(x);
-                    }
+                .iter()
+                .filter(|(_, contents, _)| bag_type.eq(contents))
+                .fold(HashSet::new(), |mut acc, (bag, ..)| {
                     acc.insert(bag);
-                    acc
+                    acc.union(&self.bags_that_contain(bag))
+                        .map(|a| *a)
+                        .collect()
                 })
         }
 
         pub fn contents(&self, bag_type: &BagType) -> i32 {
             self.matrix
                 .iter()
-                .filter(|((bag, _), _)| bag_type.eq(bag))
-                .fold(0, |acc, ((_, contents), num)| {
+                .filter(|(bag, ..)| bag_type.eq(bag))
+                .fold(0, |acc, (_, contents, num)| {
                     acc + num * (self.contents(contents) + 1)
                 })
         }
@@ -138,32 +133,48 @@ mod day07test {
     fn test_small() {
         let input = file_to_vec::<Bag>("inputs/day07small.txt");
         let mut hash_matrix = HashMatrix::new();
-        for bag in input.iter() {
-            hash_matrix.add(&bag)
-        }
-        let a = hash_matrix.bags_that_contain(&"shiny gold".parse().unwrap());
-        assert_eq!(4, a.len());
+        let a = timed!(
+            {
+                for bag in input.iter() {
+                    hash_matrix.add(&bag)
+                }
+                hash_matrix.bags_that_contain(&"shiny gold".parse().unwrap()).len()
+            },
+            "test_small"
+        );
+        assert_eq!(4, a);
     }
 
     #[test]
     fn test_large() {
         let input = file_to_vec::<Bag>("inputs/day07.txt");
         let mut hash_matrix = HashMatrix::new();
-        for bag in input.iter() {
-            hash_matrix.add(&bag)
-        }
-        let a = hash_matrix.bags_that_contain(&"shiny gold".parse().unwrap());
-        assert_eq!(124, a.len());
+        let a = timed!(
+            {
+                for bag in input.iter() {
+                    hash_matrix.add(&bag)
+                }
+                hash_matrix.bags_that_contain(&"shiny gold".parse().unwrap()).len()
+            },
+            "test_large"
+        );
+        assert_eq!(124, a);
     }
 
     #[test]
     fn test_small_p2() {
         let input = file_to_vec::<Bag>("inputs/day07small2.txt");
         let mut hash_matrix = HashMatrix::new();
-        for bag in input.iter() {
-            hash_matrix.add(&bag)
-        }
-        let a = hash_matrix.contents(&"shiny gold".parse().unwrap());
+        let a = timed!(
+            {
+                for bag in input.iter() {
+                    hash_matrix.add(&bag)
+                }
+                hash_matrix.contents(&"shiny gold".parse().unwrap())
+            },
+            "test_small_p2"
+        );
+
         assert_eq!(126, a);
     }
 
@@ -171,10 +182,15 @@ mod day07test {
     fn test_large_p2() {
         let input = file_to_vec::<Bag>("inputs/day07.txt");
         let mut hash_matrix = HashMatrix::new();
-        for bag in input.iter() {
-            hash_matrix.add(&bag)
-        }
-        let a = hash_matrix.contents(&"shiny gold".parse().unwrap());
+        let a = timed!(
+            {
+                for bag in input.iter() {
+                    hash_matrix.add(&bag)
+                }
+                hash_matrix.contents(&"shiny gold".parse().unwrap())
+            },
+            "test_large_p2"
+        );
         assert_eq!(34862, a);
     }
 }
