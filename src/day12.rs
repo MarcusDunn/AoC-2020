ff63ea0caac3bb9a9de985d2ea624539acb8c703
marcusdunn <marcus.s.dunn@gmail.com>
2020-12-12 16:46:02 -0800

    removed excess from p1

--- src/day12.rs
+++ src/day12.rs
@@ -1,43 +1,6 @@
 use std::num::ParseIntError;
-use std::ops::{Add, AddAssign, SubAssign};
+use std::ops::Add;
 use std::str::FromStr;
-
-#[derive(Copy, Clone, Debug)]
-enum Direction {
-    North,
-    South,
-    East,
-    West,
-}
-
-impl AddAssign<i32> for Direction {
-    fn add_assign(&mut self, rhs: i32) {
-        let curr = match self {
-            Direction::North => 0,
-            Direction::East => 90,
-            Direction::South => 180,
-            Direction::West => 270,
-        };
-        *self = match (rhs + curr + 360) % 360 {
-            0 => Direction::North,
-            90 => Direction::East,
-            180 => Direction::South,
-            270 => Direction::West,
-            _ => panic!(format!(
-                "{} is not 0 mod 90 on input self: {:?}, rhs: {}",
-                (rhs + curr + 360) % 360,
-                self,
-                rhs
-            )),
-        };
-    }
-}
-
-impl SubAssign<i32> for Direction {
-    fn sub_assign(&mut self, rhs: i32) {
-        *self += -rhs
-    }
-}
 
 #[derive(Debug, Copy, Clone)]
 enum Action {
@@ -70,7 +33,6 @@ impl FromStr for Action {
 #[derive(Debug)]
 struct Ship {
     position: (i32, i32),
-    direction: Direction,
     waypoint: (i32, i32),
 }
 
@@ -85,21 +47,12 @@ impl Add<Action> for Ship {
 
     fn add(self, rhs: Action) -> Self::Output {
         let mut position = self.position;
-        let mut direction = self.direction;
         let mut waypoint = self.waypoint;
         match rhs {
-            Action::North(v) => {
-                waypoint.0 += v;
-            }
-            Action::South(v) => {
-                waypoint.0 -= v;
-            }
-            Action::East(v) => {
-                waypoint.1 += v;
-            }
-            Action::West(v) => {
-                waypoint.1 -= v;
-            }
+            Action::North(v) => waypoint.0 += v,
+            Action::South(v) => waypoint.0 -= v,
+            Action::East(v) => waypoint.1 += v,
+            Action::West(v) => waypoint.1 -= v,
             Action::Right(v) => waypoint = rotate(waypoint, v),
             Action::Left(v) => waypoint = rotate(waypoint, -v),
             Action::Forward(v) => {
@@ -107,11 +60,7 @@ impl Add<Action> for Ship {
             }
         }
         //println!("due to {:?}, the ship moved from {:?} with bearing {:?} to {:?} with bearing {:?} and moved the waypoint from {:?} to {:?}", rhs, self.position, self.direction, position, direction, self.waypoint, waypoint);
-        Ship {
-            position,
-            direction,
-            waypoint,
-        }
+        Ship { position, waypoint }
     }
 }
 
@@ -127,7 +76,7 @@ fn rotate(waypoint: (i32, i32), degrees: i32) -> (i32, i32) {
 
 #[cfg(test)]
 mod tests {
-    use crate::day12::{Action, Direction, Ship};
+    use crate::day12::{Action, Ship};
     use crate::loader::file_to_vec;
 
     #[test]
@@ -136,19 +85,9 @@ mod tests {
     }
 
     #[test]
-    fn test_give_ship_direction() {
-        Ship {
-            position: (0, 0),
-            direction: Direction::North,
-            waypoint: (1, 10),
-        } + Action::Forward(10);
-    }
-
-    #[test]
     fn test_small_instr() {
         let ship = Ship {
             position: (0, 0),
-            direction: Direction::East,
             waypoint: (1, 10),
         };
         assert_eq!(
@@ -164,7 +103,6 @@ mod tests {
     fn test_instr() {
         let ship = Ship {
             position: (0, 0),
-            direction: Direction::East,
             waypoint: (1, 10),
         };
         assert_eq!(
@@ -172,7 +110,7 @@ mod tests {
                 .iter()
                 .fold(ship, |acc, x| { acc + *x })
                 .manhattan_distance(),
-            319
+            50157
         );
     }
 }
